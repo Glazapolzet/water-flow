8fa191bf9a57d69636806375404316f0b9864ec2
Glazapolzet <VypZlol@yandex.ru>
2024-08-17 14:57:06 +0300

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/water-flow/sw.js', { scope: '/water-flow/' })})}
